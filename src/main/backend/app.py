aa92181abd3c56997c1cfb825337556da482bb7b
Mohamed3873 <mohamed36912@hotmail.com>
2024-11-13 12:16:13 +0100

    added backend and frontend

---
+++ src/main/backend/app.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+load_dotenv()
+
+
+# Connect to the MySQL database
+# Connect to the MySQL database using environment variables
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),        # Cloud SQL public IP
+        user=os.getenv('DB_USER'),        # Database username
+        password=os.getenv('DB_PASSWORD'), # Database password
+        database=os.getenv('DB_NAME')     # Database name
+    )
+    return connection
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/data')
+def fetch_data():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM terraform_database.items')  # replace with your table name
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
