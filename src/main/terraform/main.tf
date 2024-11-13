af7c86be875d5604a256c4df73f3b98a2ea12f4e
Mohamed3873 <mohamed36912@hotmail.com>
2024-11-13 08:26:32 +0100

    fixed ip issues

--- src/main/terraform/main.tf
+++ src/main/terraform/main.tf
@@ -1,7 +1,7 @@
 # Configure the Google Cloud provider
 provider "google" {
-  project     = "awesome-destiny-436710-j1"
-  region      = "europe-west1"
+  project = "awesome-destiny-436710-j1"
+  region  = "europe-west1"
 }
 
 # Define the MySQL instance
@@ -9,30 +9,31 @@ resource "google_sql_database_instance" "my_mysql_instance" {
   name             = "cloud-sql"
   database_version = "MYSQL_8_0"
   region           = "europe-west1"
+
   settings {
     tier = "db-f1-micro"
-
 
     ip_configuration {
       ipv4_enabled = true
 
       authorized_networks {
         name  = "my-office-network"
-        value = " 129.241.236.225"
+        value = "129.241.236.225"
       }
     }
   }
-
 }
+
+# Define a MySQL user
 resource "google_sql_user" "my_mysql_user" {
-    instance = google_sql_database_instance.my_mysql_instance.name
-    name     = "cloud"
-    password = var.db_password
-    host     = "%"
+  instance = google_sql_database_instance.my_mysql_instance.name
+  name     = "cloud"
+  password = var.db_password
+  host     = "%"  # Allows connections from any host
 }
 
-  # Create a database in the MySQL instance
-  resource "google_sql_database" "my_database" {
-    name     = "terraform_database"                                  # Name of your database
-    instance = google_sql_database_instance.my_mysql_instance.name
-  }
+# Create a database in the MySQL instance
+resource "google_sql_database" "my_database" {
+  name     = "terraform_database"
+  instance = google_sql_database_instance.my_mysql_instance.name
+}
