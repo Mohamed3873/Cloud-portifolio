6849dc1f8824840f18ceb42bffa8fa8bf5fdcb0f
Mohamed3873 <mohamed36912@hotmail.com>
2024-11-15 16:47:13 +0100

    changed the sql ip

--- src/main/backend/app.py
+++ src/main/backend/app.py
@@ -14,7 +14,7 @@ load_dotenv()
 def get_db_connection():
     connection = mysql.connector.connect(
         host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT'),
+        port=int(os.getenv('DB_PORT', 3306)),  # Default to 3306 if DB_PORT is not set
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD'),
         database=os.getenv('DB_NAME')
