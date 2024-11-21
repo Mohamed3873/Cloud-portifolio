89272deddd11cb35c0962cdc445d503ff93ea0db
Mohamed3873 <mohamed36912@hotmail.com>
2024-11-21 12:51:31 +0100

    editet the script

--- src/main/terraform/variables.tf
+++ src/main/terraform/variables.tf
@@ -1,7 +1,29 @@
-variable "db_password" {
-  description = "The db password"
+variable "db_user" {
+  description = "The database username"
   type        = string
   sensitive   = true
 }
 
+variable "db_password" {
+  description = "The database password"
+  type        = string
+  sensitive   = true
+}
 
+variable "db_host" {
+  description = "The database host"
+  type        = string
+  sensitive   = false
+}
+
+variable "db_port" {
+  description = "The database port"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "The database name"
+  type        = string
+  sensitive   = false
+}
